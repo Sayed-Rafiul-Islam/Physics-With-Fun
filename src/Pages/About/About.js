0d378417d5044db3ad5effccb027f694aea6a2b0
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-04-17 23:02:19 +0600

    about modified

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className='container'>
-            <p className='text-light'><i>I am a beginner in this course and from the very first assignment, I have delivered exactly what I was asked to. And lately, it has been really difficult for me to maintain this course and my exams at the same time. Even after this, I am still delivering and I do understand how to code by reading documentations. So, I believe I've got the potential to become one of the <span className='text-info'>best web developers.</span> So for now, my goal is to be the best in batch 5 and then try to reach to my full potential and stand among the <span className='text-info'>bests.</span>
+            <p className='text-light'><i>I am a beginner in this course and from the very first assignment, I have delivered exactly what I was asked to. And lately, it has been really difficult for me to maintain this course and my exams at the same time. Even after this, I am still delivering and I do understand how to code by reading documentations. So, I believe I've got the potential to become one of the <span className='text-info'>best web developers.</span> So for now, my goal is to be the best in batch 5 and then try to reach to my full potential and stand among the <span className='text-info'>bests. And I believe if one really want something and work tirelessly to get it, he eventually does. So that's exactly what I'm going to do, I will turn my life upside down to stand among the bests and I will not rest until I make it happen </span>
             </i></p>
         </div>
     );
